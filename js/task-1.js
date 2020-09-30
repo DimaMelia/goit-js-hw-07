4d1877a8269d91d77eed6ce36c2ef27350fe501a
DimaMelia <64284851+DimaMelia@users.noreply.github.com>
2020-09-30 14:32:11 +0300

    ДЗ-7

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,7 @@
-const categoryItemRef = document.querySelectorAll(".item");
-console.log(`В списке ${categoryItemRef.length} категории.`);
+const categoryItemsRef = document.querySelectorAll(".item");
+console.log(`В списке ${categoryItemsRef.length} категории.`);
 
 categoryItemRef.forEach((item) => {
   console.log(`Категория: ${item.firstElementChild.textContent} 
-    Количество элементов: ${
-      document.querySelector(".item ul").children.length
-    }`);
+Количество элементов: ${item.lastElementChild.children.length}`);
 });
