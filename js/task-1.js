b74c17ad52cb03f1e97931af7bd6ffec67a65cdb
DimaMelia <64284851+DimaMelia@users.noreply.github.com>
2020-10-01 19:00:21 +0300

    update task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,5 @@
 const categoryItemsRef = document.querySelectorAll(".item");
 console.log(`В списке ${categoryItemsRef.length} категории.`);
-
 categoryItemsRef.forEach((item) => {
   console.log(`Категория: ${item.firstElementChild.textContent} 
 Количество элементов: ${item.lastElementChild.children.length}`);
